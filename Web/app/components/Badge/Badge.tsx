6c5b4aef39e941532d998757b6175967d98395ba
Gabriel Feliciano <gfp001@outlook.com>
2021-12-14 02:15:20 -0300

    Shadows in badges

--- Web/app/components/Badge/Badge.tsx
+++ Web/app/components/Badge/Badge.tsx
@@ -5,8 +5,8 @@ export interface BadgeProps {
 
 export default function Badge (props: BadgeProps) {
     return (
-        <div className="py-4 border-amber-500 border-b-4 text-amber-500 hover:scale-110">
-            <i className={`${props.icon} text-6xl text-center block`}></i>
+        <div className="py-4 border-amber-500 border-b-4 text-amber-500 hover:scale-110 shadow hover:shadow-xl">
+            <i className={`${props.icon} text-6xl text-center block drop-shadow-xl`}></i>
             <p className="block font-bold text-center text-xl select-none">{ props.text }</p>
         </div>
     );
