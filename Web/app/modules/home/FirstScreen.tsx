f87b171035d6109c823a133458f228296a225de5
Gabriel Feliciano <gfp001@outlook.com>
2021-12-15 11:15:43 -0300

    More responsive - p3

--- Web/app/modules/home/FirstScreen.tsx
+++ Web/app/modules/home/FirstScreen.tsx
@@ -24,7 +24,7 @@ const FirstScreen = (props: FirstScreenProps) => {
             className='first-screen flex flex-col px-8 md:grid gap-8 grid-cols-2 grid-rows-2 h-screen text-gray-200 bg-gray-800'
         >
             {/* Picture */}
-            <div className='hidden md:flex items-center justify-center order-2 md:order-1 relative md:row-span-2 md:order-1 lg:bg-none bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 rounded-lg'>
+            <div className='hidden order-2 md:order-1 md:flex items-center justify-center relative md:row-span-2 md:order-1 md:bg-none bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 rounded-lg'>
                 <div 
                     className='w-96 md:h-auto rounded-full overflow-hidden'
                     style={firstScreenInView ? {animation: 'picture 1.5s ease-in-out forwards'} : {}}
@@ -46,21 +46,21 @@ const FirstScreen = (props: FirstScreenProps) => {
                     }}
                     shouldAnimate={firstScreenInView}
                 >
-                    <div className='mb-4 lg:m-0'>
+                    <div className='flex'>
                         <img 
-                            className='h-32 rounded-lg float-left lg:hidden bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500'
+                            className='h-32 w-auto rounded-lg md:hidden bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500'
                             src={props.img}
                             alt="" 
                         />
-                        <h1 className='border-red-500 py-4 text-center md:text-left text-4xl font-bold md:border-l-[.25rem] md:px-8 md:text-5xl'>
+                        <h1 className='inline-block flex-grow border-red-500 py-4 text-center md:text-left text-4xl font-bold md:border-l-[.25rem] md:px-8 md:text-5xl'>
                             {props.title}
                         </h1>
                     </div>
-                    <p className='md:px-8 px-2 py-4 border-red-500 border-b-[.25rem] text-xl md:border-b-0 md:text-md lg:text-xl'>{ props.description }</p>
+                    <p className='md:px-8 p-2 border-red-500 border-b-[.25rem] text-xl md:border-b-0 md:text-md lg:text-xl'>{ props.description }</p>
                 </SliderTransition>
             </div>
             {/* Menu */}
-            <div className='order-3 flex justify-center items-center row-span-2 md:justify-start md:row-span-0'>
+            <div className='flex order-3 justify-center items-center row-span-2 md:justify-start md:row-span-0'>
                 <AnchorMenu buttons={props.menuButtons} />
             </div>
         </section>
